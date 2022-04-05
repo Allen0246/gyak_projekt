0f05f077d2d1456b2d5b17f4eb6dd6960230b3fb
Kelemen Richárd <kelemen.richard@acenetwork.hu>
2022-04-05 10:42:59 +0200

    pass_prog mappa hozzáadása

---
+++ python_pass_prog/pass_prog.py
@@ -0,0 +1,19 @@
+
+
+jelszo = 'neaddki'
+
+bemenet = input('Mi a jelszo? ')
+
+proba = 0 
+
+
+while bemenet !=jelszo:
+    proba +=1
+    if proba == 3:
+        print('Rendszer lezarva')
+        break
+    print('Rossz jelszo, problad ujra ')
+    bemenet = input('Mi a jelszo? ')
+
+if bemenet == jelszo:
+    print('Hozzaferes megadva. ')
