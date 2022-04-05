8a986bc5588aee079a07f5864f0e64e8e2c9dcbc
Kelemen Richárd <kelemen.richard@acenetwork.hu>
2022-04-05 12:00:56 +0200

    fgnyek 2. példa

--- gyakorlatok/fuggvenyek.py
+++ gyakorlatok/fuggvenyek.py
@@ -11,4 +11,31 @@ def nev_printer(nev_lista):
 nev_printer(nevek1)
 nev_printer(nevek2)
 
-#commit , majd push
+#commit , majd push 1. része
+
+a = 10
+b = 20
+
+def osszeadas1():
+    print(a+b)
+
+def osszeadas2(a,b, c=4):
+    return a+b+c 
+
+def osszeadas3(*barmi):
+    return sum(barmi) #célszerű args ot használni a bármi helyett.
+
+def udvozlesek(*args):
+    koszones = 'Ennyi féle köszönés létezik: '
+    for k in args:
+        koszones += k
+        koszones += ', '
+    print(koszones[0:len(koszones)-2])
+
+udvozlesek('szia', 'szervusz', 'hello','szevasz')
+
+osszeadas1()
+osszeg =  osszeadas2(45, 25)
+print(osszeg)
+
+print (osszeadas3(10,20,30,40,50,60,70,80,90,100))
