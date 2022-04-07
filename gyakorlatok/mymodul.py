098e37d7ac11126225480193c2cfdf1f15843633
Kelemen Richárd <kelemen.richard@acenetwork.hu>
2022-04-07 18:13:22 +0200

    if_name_eq_main

---
+++ gyakorlatok/mymodul.py
@@ -0,0 +1,20 @@
+def hello():
+    print(f'Hello {__name__} modulból!')
+
+szam= 10
+
+class személy:
+    def __init__(self,név,kor):
+        self.név = név
+        self.kor = kor
+    
+    def show_fields(self)
+        print(self.név, self.kor)
+
+
+
+if __name__ == '__main__':
+    hello()
+    print(szam)
+    Juli = személy('Juliska',25)
+    Juli.show_fields()
