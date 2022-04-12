19cedd303002865818567f7efd0be4390625e54c
Kelemen Richárd <kelemen.richard@acenetwork.hu>
2022-04-12 19:20:41 +0200

    xml fájl előállítása xlsx-ből

--- excelbol_xmlbe/excel_to_xml.py
+++ excelbol_xmlbe/excel_to_xml.py
@@ -2,7 +2,7 @@ from sqlite3 import Row
 import pandas as pd
 
 # beolvassa az excel file-t
-df = pd.read_excel('excelbol_xmlbe/adat1.xlsx')
+df = pd.read_excel('adat1.xlsx')
 print(df)
 
 #csinál egy függvényt amiben betölti az adatok egy xml fájlba
@@ -10,13 +10,13 @@ def to_xml(df):
     def row_xml(row):
         xml = ['<item>']
         for i ,col_name in enumerate(row.index):
-            xml.append('<{0}>{1}</{0}>'.format(col_name,row.iloc[i]))
+            xml.append(' <{0}>{1}</{0}>'.format(col_name, row.iloc[i]))
         xml.append('</item>')
         return '\n'.join(xml)
     res='\n'.join(df.apply(row_xml, axis=1))
     return(res)
-
 to_xml(df)
+
     # elvileg létrekéne hozza az xml fájlt
-with open('df.XML','w') as f:
-     f.write(to_xml(df))
+with open('df.XML','w') as file:
+     file.write(to_xml(df))
