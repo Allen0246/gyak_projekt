34c68983eb9d8b6be1a6c2936d83e558907b8680
Kelemen Richárd <kelemen.richard@acenetwork.hu>
2022-04-12 18:51:35 +0200

    excel fájl betöltése xml-be majd létrehozása

--- excelbol_xmlbe/excel_to_xml.py
+++ excelbol_xmlbe/excel_to_xml.py
@@ -1,8 +1,22 @@
+from sqlite3 import Row
 import pandas as pd
 
+# beolvassa az excel file-t
+df = pd.read_excel('excelbol_xmlbe/adat1.xlsx')
+print(df)
 
-read1 = pd.read_excel('excelbol_xmlbe/adat1.xlsx')
-print(read1)
-toxml = pd.DataFrame(read1)
+#csinál egy függvényt amiben betölti az adatok egy xml fájlba
+def to_xml(df):
+    def row_xml(row):
+        xml = ['<item>']
+        for i ,col_name in enumerate(row.index):
+            xml.append('<{0}>{1}</{0}>'.format(col_name,row.iloc[i]))
+        xml.append('</item>')
+        return '\n'.join(xml)
+    res='\n'.join(df.apply(row_xml, axis=1))
+    return(res)
 
-xml_write=open('toxml.xml' 'w').write()
+to_xml(df)
+    # elvileg létrekéne hozza az xml fájlt
+with open('df.XML','w') as f:
+     f.write(to_xml(df))
